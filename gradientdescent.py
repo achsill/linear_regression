c85321ccc33b7870bfc89ce8986256df4255781d
Hamza LOUAR <hlouar@e3r7p1.42.fr>
2019-06-09 17:08:39 +0200

    started predict file, still need to denormalize the slope

--- gradientdescent.py
+++ gradientdescent.py
@@ -3,43 +3,42 @@ import matplotlib.pyplot as plt
 import statistics
 import numpy as np
 import math
-from sklearn.preprocessing import normalize
 
 
 df = pd.read_csv('data.csv')
 x = df.iloc[:,0]
 y = df.iloc[:,1]
-print(x)
-tmp_x = x
+min_x = min(x)
+max_x = max(x)
+
 def normalization(value):
-    return (value - min(tmp_x)) / (max(tmp_x) - min(tmp_x))
+    return (value - min_x) / (max_x - min_x)
 
 def denormalization(value):
-    return (value * (max(tmp_x) - min(tmp_x)) + min(tmp_x))
+    return (value * (max_x - min_x) + min_x)
 
-m_curr = b_curr = 0
-iterations = 50000
-n = len(x)
+m = 0
+b = 0
+iterations = 100000
+n = float(len(x))
 learning_rate = 0.01
 x = normalization(x)
+tmp_cost = 0
 for i in range(iterations):
-    y_predicted = m_curr * x + b_curr
+    y_predicted = m * x + b
     cost = (1/n) * sum([val ** 2 for val in (y - y_predicted)])
-    md = (1/(2*n))*sum((y_predicted - y)*x)
-    bd = (1/(2*n))*sum(y_predicted - y)
-    m_curr = m_curr - learning_rate * md
-    b_curr = b_curr - learning_rate * bd
+    if (abs(tmp_cost - cost) < 0.001):
+        break
+    tmp_m = (1/n) * sum((y_predicted - y) * x)
+    tmp_b = (1/n) * sum(y_predicted - y)
+    m = m - learning_rate * tmp_m
+    b = b - learning_rate * tmp_b
+    tmp_cost = cost
 
-# print(m_curr, b_curr)
-print(x)
-
-testX = normalization(230000)
-
-testY = m_curr * testX + b_curr
-print(testY)
+# np.save("train_result", [m, b])
 
 xDraw = np.linspace(min(x),max(x),1000)
-yDraw = m_curr * xDraw + b_curr
+yDraw = m * xDraw + b
 plt.scatter(x, y)
 plt.plot(xDraw, yDraw)
 plt.show()
