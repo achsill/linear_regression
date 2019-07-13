ca9972d35417d7bd615ff5d6f50a05a9c7e9a08a
hlouar <hamzalouar@gmail.com>
2019-07-13 13:23:04 +0200

    everything OK, need to create functions

--- predict.py
+++ predict.py
@@ -1,11 +1,12 @@
 import numpy as np
 import pandas as pd
+import math as Math
 
 train_result = np.load("train_result.npy")
 
 while(True):
     mileage = input("Mileage: ")
     if not mileage.lower() == "exit":
-        print(train_result[0] * float(mileage) + train_result[1])
+        print("{}{}".format(round(train_result[0] * float(mileage) + train_result[1], 2), "$"))
     else:
         break
